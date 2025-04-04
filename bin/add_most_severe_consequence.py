488c612831659921037c927c917f8861ae66a3b2
Alexander Koc <alexander.koc@skane.se>
2025-04-04 08:44:34 +0200

    fix unbound var error if no CSQ

--- bin/add_most_severe_consequence.py
+++ bin/add_most_severe_consequence.py
@@ -68,9 +68,17 @@ def construct_most_severe_consequence_info(
 
     columns = line.strip().split()
     info_fields = columns[7].split(";")
+
+    transcripts = None
+
     for field in info_fields:
         if field.startswith("CSQ="):
             transcripts = field.split("CSQ=")[1].split(",")
+            break
+
+    if transcripts is None:
+        transcripts = []
+
     hgnc_ids, alleles, consequences, severity = parse_vep_csq_transcripts(
         transcripts, allele_ind, csq_ind, hgnc_ind, var_csq
     )
